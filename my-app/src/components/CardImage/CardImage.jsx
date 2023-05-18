8e8fc8277ea64d673136d8b2708d7e68aa2b2d6b
mogisilta <201715516@jbnu.ac.kr>
2023-05-18 23:47:11 +0900

    최종수정

--- my-app/src/components/CardImage/CardImage.jsx
+++ my-app/src/components/CardImage/CardImage.jsx
@@ -6,7 +6,8 @@ import styles from "./CardImage.module.css";
 import ItemType from "../ItemCard/ItemCard";
 export default function CardImage({item}) {
     const dispatch = useDispatch();
-    const handleUpdate = (id,bookMark) => {
+    const handleUpdate = (id,bookMark,event) => {
+        event.stopPropagation();
         const isMarked = bookMark;
         isMarked ? dispatch(showToast('북마크 삭제 됐어요')) : dispatch(showToast('북마크 추가 됐어요'));
         dispatch(update(id));
@@ -15,7 +16,7 @@ export default function CardImage({item}) {
     return(
         <div className={styles.imageContainer}>
             <img className={styles.image} src={image_url ? image_url : brand_image_url} alt={ItemType.Category}/>
-            <AiFillStar className={bookMark ? styles.starfilled : styles.star} onClick={()=>handleUpdate(id,bookMark)}/>
+            <AiFillStar className={bookMark ? styles.starfilled : styles.star} onClick={(event)=>handleUpdate(id,bookMark,event)}/>
         </div>
     )
 }
